5fbb64fbfa1d81b3baa8b1e78a5feeeadf667b7a
Emilli-Jiully <128311260+Emilli-Giuliane@users.noreply.github.com>
2023-06-23 20:11:55 -0300

    Update Atv27.java

--- Treinando-Java/ListasDeAtividades/PythonBrasil/2EstruturaDeDecisao/Atv27.java
+++ Treinando-Java/ListasDeAtividades/PythonBrasil/2EstruturaDeDecisao/Atv27.java
@@ -1,39 +1,47 @@
-/*Uma fruteira está vendendo frutas com a seguinte tabela       * de preços:
-Até 5 Kg           Acima de 5 Kg
+/*Uma fruteira está vendendo frutas com a seguinte tabela       
+* de preços:Até 5 Kg           Acima de 5 Kg
 Morango         R$ 2,50 por Kg          R$ 2,20 por Kg
 Maçã            R$ 1,80 por Kg          R$ 1,50 por Kg
  * 
  * Se o cliente comprar mais de 8 Kg em frutas ou o valor total da compra ultrapassar R$ 25,00, receberá ainda um desconto de 10% sobre este total. Escreva um algoritmo para ler a quantidade (em Kg) de morangos e a quantidade (em Kg) de maças adquiridas e escreva o valor a ser pago pelo cliente.
 */
 import java.util.Scanner;
+
 public class Atv27 {
     public static void main(String[] args) {
-        Scanner read = new Scanner(System.in);
-        System.out.println("Insira a quantidade (em Kg) de morangos: ");
-        double pesoMorango = read.nextDouble();
-        System.out.println("Insira a quantidade (em kg) de maçãs: ");
-        double pesoMaca = read.nextDouble();
-        read.close();
-        double precoMorango = 0;
-        double precoMaca = 0; 
-        double pesoTotal = 0;
-        double precoTotal = 0; 
-        
-        if (pesoMorango <= 5) { 
-            precoMorango = 2.5 * pesoMorango;
-        } else if (pesoMorango > 5) {
-            precoMorango = 2.2 * pesoMorango;
+      Scanner scanner = new Scanner(System.in);
+      
+      double kgMaca = 0,kgMorango = 0,preco = 0,precoPagar = 0,desconto;
+      int respA,respB;
+      System.out.println("Bem-Vindo! Qual sua preferencia de fruta para comprar em kg? Por favor, digite o número '1' para maçã e ou digite '2' para morango");
+      respA = scanner.nextInt();
+    System.out.println("Qual a quantidade em peso (kg) de sua preferência para a compra? ");
+      respB = scanner.nextInt();
+      scanner.close();
+
+      if (respA == 1) {
+        if (respB > 0 && respB <= 5) {
+          kgMaca = 1.80;
+          preco = kgMaca * respB;
+        } else if (respB > 5) {
+          kgMaca = 1.50;
+          preco = kgMaca * respB;
         }
-        if (pesoMaca <= 5) {
-            precoMaca = 1.8 * pesoMaca;
-        } else if (pesoMaca > 5) {
-            precoMaca = 1.5 * pesoMaca;
-        }
-        pesoTotal = pesoMorango + pesoMaca;
-        precoTotal = precoMaca + precoMorango;
-        if (pesoTotal > 8 || precoTotal > 25) {
-            precoTotal = precoTotal * 0.9;
-        }
-        System.out.printf("Valor a pagar: R$%.2f\n" , precoTotal);
+      } if (respA == 2) {
+        if (respB > 0 && respB <= 5) {
+          kgMorango = 2.50;
+          preco = kgMorango * respB;
+        } else if (respB > 5) {
+          kgMorango = 2.20;
+          preco = kgMorango * respB;
+        } 
+      } precoPagar = preco;
+      
+      if (precoPagar >= 25.00 || respB >= 8){
+        desconto = (precoPagar * 0.1) * precoPagar;
+        System.out.println("Você recebeu um desconto de 10%!!! O valor a pagar com o desconto é de: R$" + desconto);
+      } else {
+        System.out.println("O valor a pagar é de: R$" + precoPagar);
+      }
     }
-}
+}//não curti o resultado então refiz do zero. Agora refeito acho que me agrada mais.
