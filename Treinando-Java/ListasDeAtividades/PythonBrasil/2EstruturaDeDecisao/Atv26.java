b5f1be497429a64cdc497e31e0025de75a794992
Emilli-Jiully <128311260+Emilli-Giuliane@users.noreply.github.com>
2023-06-23 19:20:05 -0300

    Update Atv26.java

--- Treinando-Java/ListasDeAtividades/PythonBrasil/2EstruturaDeDecisao/Atv26.java
+++ Treinando-Java/ListasDeAtividades/PythonBrasil/2EstruturaDeDecisao/Atv26.java
@@ -7,7 +7,6 @@
  * Escreva um algoritmo que leia o número de litros vendidos, o tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina), calcule e imprima o valor a ser pago pelo cliente sabendo-se que o preço do litro da gasolina é R$ 2,50 o preço do litro do álcool é R$ 1,90.
 */
 import java.util.Scanner;
-
 public class Atv26 {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
@@ -17,7 +16,6 @@ public class Atv26 {
         System.out.println("Informe a quantidade de litros vendidos: ");
         double litroVendido = read.nextDouble();
         read.close();
-        double pago = 0;
         double valorPagar = 0;
         double precoA = 1.90;
         double precoG = 2.50;
@@ -37,7 +35,7 @@ public class Atv26 {
                 valorPagar = (litroVendido - 20) * 2.35;
             } valorPago = valorPagar + precoG;
         }//O mesmo aqui
-      valorPago = valorPagar + precoA;
+      
         System.out.println("O valor que deve ser pago será de: R$" +valorPago);
     }
 }//não estava funcionando de jeito nenhum, então decidi consertar.
